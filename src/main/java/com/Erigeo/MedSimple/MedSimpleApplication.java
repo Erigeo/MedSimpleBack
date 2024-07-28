ffd176761312d955c9682921be505ae81be3cc20
Georg Herison <georgherison@gmail.com>
2024-07-27 22:36:40 -0300

    starting...

---
+++ src/main/java/com/Erigeo/MedSimple/MedSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.Erigeo.MedSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Erigeo/MedSimple/MedSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Erigeo.MedSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
